d9b28f0c53e4f238e9fc7d7528f1a5008e77d8a0
dichha <dichha-rai@uiowa.edu>
2017-08-10 18:39:04 -0500

    added `api_root` method and SnippetHighlight class

--- snippets/views.py
+++ snippets/views.py
@@ -16,7 +16,10 @@ from rest_framework.parsers import JSONParser
 #from rest_framework.views import APIView
 #from rest_framework import mixins
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from snippets.models import Snippet 
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
@@ -28,7 +31,7 @@ class SnippetList(generics.ListCreateAPIView):
 	serializer_class = SnippetSerializer
 	permission_classes = (permissions.IsAuthenticatedOrReadOnly, \
 		IsOwnerOrReadOnly)
-	
+
 	def perform_create(self, serializer): 
 		serializer.save(owner=self.request.user)
 
@@ -45,6 +48,21 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView): 
 	querset = User.objects.all()
 	serializer_class = UserSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None): 
+	return Response({
+		'users': reverse('user-list', request=request, format=format),
+		'snippets': reverse('snippet-list', request=request, format=format)
+		})
+
+class SnippetHighLight(generics.GenericAPIView): 
+	queryset = Snippet.objects.all()
+	renderer_classes = (renderers.StaticHTMLRenderer,)
+
+	def get(self, request, *args, **kwargs): 
+		snippet = self.get_object()
+		return Response(snippet.highlighted)
 
 	
 '''
