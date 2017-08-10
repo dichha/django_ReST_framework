139ecb8b226ea7526df0c256fe649cb9d8d6dfe6
dichha <dichha-rai@uiowa.edu>
2017-08-10 13:42:31 -0500

    added permissions and User class views * added permission to SnipperList and SnippetDetail class views * added UserList and UserDetail class views

--- snippets/views.py
+++ snippets/views.py
@@ -15,17 +15,37 @@ from rest_framework.parsers import JSONParser
 #from rest_framework.response import Response
 #from rest_framework.views import APIView
 #from rest_framework import mixins
-from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
 from snippets.models import Snippet 
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
+
+
 
 class SnippetList(generics.ListCreateAPIView): 
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly, \
+		IsOwnerOrReadOnly)
+	
+	def perform_create(self, serializer): 
+		serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView): 
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly, \
+		IsOwnerOrReadOnly)
+
+class UserList(generics.ListAPIView): 
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView): 
+	querset = User.objects.all()
+	serializer_class = UserSerializer
+
 	
 '''
 class SnippetList(mixins.ListModelMixin,
