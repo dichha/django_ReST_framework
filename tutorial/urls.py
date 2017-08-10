a4ed9f20bdb4e5cb5b7324f113fd8a6016ad7f37
dichha <dichha-rai@uiowa.edu>
2017-08-10 18:45:41 -0500

    Added comment for url of `api_auth`

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -28,6 +28,7 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
 	url(r'^admin/', admin.site.urls), 
 	url(r'^', include(router.urls)), 
+    # Login and logout views for the browsable API
 	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include('snippets.urls')),
 ]
