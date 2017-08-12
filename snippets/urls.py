4152fb261accaecdbb5ca2156d7e625eb67062bf
dichha <dichha-rai@uiowa.edu>
2017-08-12 17:52:03 -0500

    added urls for schema

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,15 +4,19 @@ from snippets import views
 from snippets.views import SnippetViewSet, UserViewSet, api_root
 from rest_framework import renderers
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 # Create a router and register our viewsets with it
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
+schema_view = get_schema_view(title='Pastebin API')
+
 # The  API are now determined automatically by the router.
 urlpatterns = [
 	url(r'^', include(router.urls)), 
+	url(r'^schema/$', schema_view),
 ]
 
 '''
